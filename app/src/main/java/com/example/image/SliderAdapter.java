6eff4c38497d7d6ca5aa6784c8078f75631108c2
angyy11 <angyy-wm19@student.tarc.edu.my>
2020-08-23 21:48:10 +0800

    Initial commit

---
+++ app/src/main/java/com/example/image/SliderAdapter.java
@@ -0,0 +1,69 @@
+package com.example.image;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+import java.util.List;
+
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder>{
+
+    private List<SliderItem> sliderItem;
+    private ViewPager2 viewPager2;
+
+    SliderAdapter(List<SliderItem> sliderItem, ViewPager2 viewPager2) {
+        this.sliderItem = sliderItem;
+        this.viewPager2 = viewPager2;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new SliderViewHolder(
+            LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.slide_item_container,parent,false
+            )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+       holder.setImage(sliderItem.get(position));
+       if (position == sliderItem.size()-2){
+           viewPager2.post(runnable);
+       }
+    }
+
+    @Override
+    public int getItemCount() {
+        return sliderItem.size();
+    }
+
+    class SliderViewHolder extends RecyclerView.ViewHolder{
+        private RoundedImageView imageView;
+
+        SliderViewHolder(@NonNull View itemView){
+            super(itemView);
+            imageView=itemView.findViewById(R.id.imageSlide);
+
+        }
+
+        void setImage(SliderItem sliderItem){
+            imageView.setImageResource(sliderItem.getImage());
+        }
+    }
+
+    private  Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItem.addAll(sliderItem);
+            notifyDataSetChanged();
+        }
+    };
+}
---
+++ app/src/main/java/com/example/image/SliderItem.java
@@ -0,0 +1,12 @@
+package com.example.image;
+
+public class SliderItem {
+    private int image;
+
+    SliderItem(int image){
+        this.image=image;
+    }
+    public int getImage(){
+        return image;
+    }
+}
